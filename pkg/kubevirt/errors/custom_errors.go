d326bf030fe1cf29ab487a49f5347e566f8eef86
Moath Qasim <moad.qassem@gmail.com>
2020-08-05 13:17:40 +0200

    Adding Kubevirt Data Volume IsNotFound custom error
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

--- pkg/kubevirt/errors/custom_errors.go
+++ pkg/kubevirt/errors/custom_errors.go
@@ -24,3 +24,24 @@ func IsMachineNotFoundError(err error) bool {
 		return false
 	}
 }
+
+// DataVolumeError is used to indicate not found error in Kubevirt DataVolumeManager
+type DataVolumeError struct {
+	// Name is the machine name
+	Name string
+	// Namespace is the datavolume namespace
+	Namespace string
+}
+
+func (e *DataVolumeError) Error() string {
+	return fmt.Sprintf("data volume name=%s in namespace=%s is not found", e.Name, e.Namespace)
+}
+
+func IsDataVolumeError(err error) bool {
+	switch err.(type) {
+	case *DataVolumeError:
+		return true
+	default:
+		return false
+	}
+}
